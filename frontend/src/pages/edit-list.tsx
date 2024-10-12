a53f88e42797f4677e319d09308d6e321dcdc317
Maximilian Lösch <m.loesch@hotmail.de>
2024-10-12 18:09:20 +0200

    revert abstraction of editable list

--- frontend/src/pages/edit-list.tsx
+++ frontend/src/pages/edit-list.tsx
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { ListItem } from '../types/types.ts'
 import { addItemToList, getListItems, removeItemFromList } from '../api/api.ts'
-import EditableList from '../elements/editable-list.tsx'
 
 
 const EditList = () => {
@@ -8,11 +9,66 @@ const EditList = () => {
   if (!listId) {
     throw new Error('listId must be present')
   }
+  const [ listItems, setListItems ] = useState<ListItem[]>([])
+  const [ newItemName, setNewItemName ] = useState<string>('')
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const items = await getListItems(listId)
+        setListItems(items)
+      } catch (error) {
+        console.error('Error fetching list items', error)
+      }
+    })()
+  }, [ listId ])
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault()
+    if (!newItemName) {
+      return
+    }
+    try {
+      const newItem: ListItem = await addItemToList(newItemName, listId)
+      setListItems([ ...listItems, newItem ])
+      setNewItemName('')
+      event.target.reset()
+    } catch (error) {
+      console.error('There was a problem adding a new item', error)
+    }
+  }
+
+  const removeItem = async (itemId: string) => {
+    try {
+      await removeItemFromList(itemId, listId)
+      setListItems(listItems.filter(item => item.id !== itemId))
+      console.log('Item removed')
+    } catch (error) {
+      console.error('There was a problem removing the item', error)
+    }
+  }
 
   return (
-    <EditableList title="Liste Bearbeiten" getItemsApiCall={ getListItems } addItemApiCall={ addItemToList }
-                  removeItemApiCall={ removeItemFromList } listId={listId}/>
+    <div>
+      <h1>Liste Bearbeiten</h1>
+      <div className="listAndInput">
+        <div className="listContainer">
+          { listItems.map((item, index) => (
+            <div key={index} className="listElementContainer">
+              <div className="listElement">
+                { item.name }
+              </div>
+              <button className="deleteButton" onClick={ () => removeItem(item.id) }><img src="/paper-bin.svg" alt="delete item"/>
+              </button>
+            </div>
+          )) }
+        </div>
+        <form className="addItemForm" onSubmit={ handleSubmit }>
+          <input type="text" onChange={ e => setNewItemName(e.target.value) }/>
+          <button className="addButton small" type="submit">Hinzufügen</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/edit-staples.tsx
+++ frontend/src/pages/edit-staples.tsx
@@ -1,11 +1,69 @@
+import { useEffect, useState } from 'react'
+import { ListItem } from '../types/types.ts'
 import { createStaple, deleteStaple, getStaples } from '../api/api.ts'
-import EditableList from '../elements/editable-list.tsx'
 
 
 const EditStaples = () => {
+  const [ staples, setStaples ] = useState<ListItem[]>([])
+  const [ newStapleName, setNewStapleName ] = useState<string>('')
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const items = await getStaples()
+        setStaples(items)
+      } catch (error) {
+        console.error('Error fetching staples', error)
+      }
+    })()
+  }, [])
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault()
+    if (!newStapleName) {
+      return
+    }
+    try {
+      const newItem: ListItem = await createStaple(newStapleName)
+      setStaples([ ...staples, newItem ])
+      setNewStapleName('')
+      event.target.reset()
+    } catch (error) {
+      console.error('There was a problem adding a new staple', error)
+    }
+  }
+
+  const removeItem = async (itemId: string) => {
+    try {
+      await deleteStaple(itemId)
+      setStaples(staples.filter(item => item.id !== itemId))
+      console.log('Item removed')
+    } catch (error) {
+      console.error('There was a problem removing the staple', error)
+    }
+  }
+
   return (
-    <EditableList title="Staples" getItemsApiCall={ getStaples } addItemApiCall={ createStaple }
-                  removeItemApiCall={ deleteStaple } />
+    <div>
+      <h1>Staples</h1>
+      <div className="listAndInput">
+        <div className="listContainer">
+          { staples.map((item, index) => (
+            <div key={index} className="listElementContainer">
+              <div className="listElement">
+                { item.name }
+              </div>
+              <button className="deleteButton" onClick={ () => removeItem(item.id) }><img src="/paper-bin.svg" alt="delete item"/>
+              </button>
+            </div>
+          )) }
+        </div>
+        <form className="addItemForm" onSubmit={ handleSubmit }>
+          <input type="text" onChange={ e => setNewStapleName(e.target.value) }/>
+          <button className="addButton small" type="submit">Hinzufügen</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
