0f2a425e52ccc1f9c57d3f8be9eb5fa31c5823f9
Maximilian Lösch <m.loesch@hotmail.de>
2024-10-26 11:26:24 +0200

    cleanup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
-import { join } from 'path'
 import cookieParser from 'cookie-parser'
 import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
-  app.useStaticAssets(join(__dirname, '..', 'public'))
-  app.setBaseViewsDir(join(__dirname, '..', 'views'))
-  app.setViewEngine('hbs')
   app.use(cookieParser())
   app.enableCors({
     origin: app.get(ConfigService).get('FRONTEND_HOST'),
