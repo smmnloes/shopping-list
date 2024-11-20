0eb635af7091558e7fa2fb8df8952ef54f6fafd0
Maximilian Lösch <m.loesch@hotmail.de>
2024-11-20 22:05:42 +0100

    allow bigger payloads

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { NestExpressApplication } from '@nestjs/platform-express'
 import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 import { ConfigService } from '@nestjs/config'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
   app.use(cookieParser())
+  app.use(bodyParser.json({ limit: '50mb' }))
   app.enableCors({
     origin: app.get(ConfigService).get('FRONTEND_HOST'),
     methods: [ 'GET', 'POST', 'DELETE' ],
