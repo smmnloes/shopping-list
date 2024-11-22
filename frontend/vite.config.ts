006258ea19edd3de0431ec54fdd4ae8be602a349
Maximilian Lösch <m.loesch@hotmail.de>
2024-11-22 16:44:32 +0100

    deactivate sourcemaps

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    sourcemap: true
+    sourcemap: false,
   },
   plugins: [ react(), VitePWA({
     registerType: 'autoUpdate',
