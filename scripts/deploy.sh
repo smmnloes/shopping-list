462ea935997748c52fc69926295697b6f8f8b48b
Maximilian Lösch <m.loesch@hotmail.de>
2024-11-24 18:41:06 +0100

    build locally

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,17 +2,15 @@
 npm version minor
 git push
 
-ssh -t ubuntu@mloesch.it << 'EOF'
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-
-cd /var/www/shopping-list
-git pull
-
+cd ..
 npm run install
 npm run build
 
+ rsync -av --delete --progress ./backend/dist ubuntu@mloesch.it:/var/www/shopping-list/backend/dist
+ rsync -av --delete --progress ./frontend/dist ubuntu@mloesch.it:/var/www/shopping-list/frontend/dist
+
+ssh -t ubuntu@mloesch.it << 'EOF'
+#!/bin/bash
 sudo systemctl daemon-reload
 sudo systemctl restart shopping-list-backend
 EOF
