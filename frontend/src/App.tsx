c80f1668ddaaa37773d50f6c6b30560f804d1b9c
Maximilian Lösch <maximilian.loesch@europace.de>
2024-12-02 22:21:47 +0100

    home screen revamp

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,25 @@ function App() {
 
   return (
     <>
-        <h1>Einkaufsliste</h1>
         <nav>
           <div className="navContainer">
-            <div className="navElement small"><Link to="/edit-lists"><img src="/shopping-list-icon.svg"
-                                                                          alt="einkaufsliste"/></Link></div>
-            <div className="navElement"><Link to="/meal-plan"><img src="/meal.svg" alt="meal-plan"/></Link></div>
-            <div className="navElement"><Link to="/notes"><img src="/notebook.svg" alt="notes"/></Link></div>
+              <div className="navElement small">
+                  <Link to="/edit-lists"><img src="/shopping-list-icon.svg"
+                                              alt="einkaufsliste"/>
+                  </Link>
+                  <span>Einkaufsliste</span>
+              </div>
+
+              <div className="navElement">
+                  <Link to="/meal-plan"><img src="/meal.svg" alt="meal-plan"/>
+                  </Link>
+                  <span>Meal Plan</span>
+              </div>
+              <div className="navElement">
+                  <Link to="/notes"><img src="/notebook.svg" alt="notes"/>
+                  </Link>
+                  <span>Notizen</span>
+              </div>
           </div>
         </nav>
     </>
