528ebca076a700f248827cbf546ad6f8ea448d44
Maximilian Lösch <maximilian.loesch@europace.de>
2024-12-05 18:06:33 +0100

    Revert "env"
    This reverts commit 31821355481074dbc6c4fc2f6d11161f2bd6c6f9.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,7 @@ import { ApiModule } from './api/api.module'
 @Module({
   imports: [ ConfigModule.forRoot({
     isGlobal: true,
+    envFilePath: process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env'
   }), ApiModule ],
   controllers: [],
   providers: [],
