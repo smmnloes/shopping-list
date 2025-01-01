313b5096d063276ce0ecb2c623ce6f90cfa96205
Maximilian Lösch <m.loesch@hotmail.de>
2025-01-01 16:27:10 +0100

    button for note sorting

--- frontend/src/pages/notes.tsx
+++ frontend/src/pages/notes.tsx
@@ -4,6 +4,24 @@ import { useNavigate } from 'react-router-dom'
 import type { NoteOverview } from '../../../shared/types/notes.ts'
 
 
+const SORT_CRITERIA = [ 'CREATION_TIME', 'EDIT_TIME', 'ALPHABETICAL' ] as const
+type SORT_CRITERIA = (typeof SORT_CRITERIA)[number]
+
+const SORT_ORDER = [ 'DESCENDING', 'ASCENDING' ] as const
+type SORT_ORDER = (typeof SORT_ORDER)[number]
+
+const iconForSortCriteria: { [sortCriteria in SORT_CRITERIA]: string } = {
+  EDIT_TIME: '/edit-document.svg',
+  CREATION_TIME: '/add-document.svg',
+  ALPHABETICAL: '/sort-alphabet.svg'
+}
+
+const iconForSortOrder: { [sortOrder in SORT_ORDER]: string } = {
+  ASCENDING: '/sort-up.svg',
+  DESCENDING: '/sort-down.svg',
+
+}
+
 const formatDate = (date: Date): string => {
   return `${ date.toLocaleDateString(undefined, {
     day: '2-digit',
@@ -18,6 +36,8 @@ const sortNotesByCreatedDescending = (notes: NoteOverview[]) => [ ...notes.sort(
 
 const Notes = () => {
   const [ notes, setNotes ] = useState<NoteOverview[]>([])
+  const [ sortCriteria, setSortCriteria ] = useState(0)
+  const [ sortOrder, setSortOrder ] = useState(0)
 
   const navigate = useNavigate()
 
@@ -34,13 +54,27 @@ const Notes = () => {
   return (
     <div>
       <h1>Notizen</h1>
+
       <div className="listContainer notes">
+        <div className="notesListControls">
+          <button className="my-button" onClick={ () => {
+            setSortOrder((before) => (before + 1) % SORT_ORDER.length)
+          } }><img src={ iconForSortOrder[SORT_ORDER[sortOrder]] } alt="Sorting order"/></button>
+
+          <button className="my-button multi-img" onClick={ () => {
+            setSortCriteria(before => (before + 1) % SORT_CRITERIA.length)
+          } }>
+            <img src={ iconForSortCriteria[SORT_CRITERIA[sortCriteria]] } alt="Sorting criteria"/>
+          </button>
+        </div>
+
         <div className="listElement newElement" onClick={ newNoteHandler }>Neue Notiz</div>
         { notes.map((note, index) => (
           <div key={ index } className="listElementContainer">
             <div className="listElement" onClick={ () => navigate(`/notes/${ note.id }`) }>
               <div className="noteContainer">
-                <img className="visibilityIcon" hidden={ note.publiclyVisible } src="/padlock-locked.svg" alt='Note is private'/>
+                <img className="visibilityIcon" hidden={ note.publiclyVisible } src="/padlock-locked.svg"
+                     alt="Note is private"/>
                 <div className="noteTitle">{ note.title }</div>
                 <div className="noteDetails">
                   <div><b>erstellt:</b> { formatDate(new Date(note.createdAt)) } ({ note.createdBy })</div>
