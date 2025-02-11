31cba6615217654606e483bf51a8476a7c118dcc
Maximilian Lösch <m.loesch@hotmail.de>
2025-02-11 21:24:52 +0100

    going well

--- frontend/src/api/file.ts
+++ frontend/src/api/file.ts
@@ -1,28 +1,15 @@
 import { axiosInstance, backendHost } from './api.ts'
-import { AxiosRequestConfig } from 'axios'
+import { AxiosProgressEvent } from 'axios'
 
-export const uploadFiles = async (file: File): Promise<void> => {
-  // Create form data
+export const uploadFiles = async (file: File, onProgress: (event: AxiosProgressEvent) => void, abortController: AbortController): Promise<void> => {
   const formData = new FormData();
   formData.append('file', file);
 
-  // Additional metadata if needed
-  formData.append('filename', file.name);
-  formData.append('contentType', file.type);
-
-  // Configure axios request
-  const config: AxiosRequestConfig = {
+  return axiosInstance.post(`${ backendHost }/api/files`, formData, {
+    signal: abortController.signal,
     headers: {
       'Content-Type': 'multipart/form-data',
     },
-    // onUploadProgress: (progressEvent: UploadProgressEvent) => {
-    //   if (onProgress) {
-    //     const percentage = Math.round(
-    //       (progressEvent.loaded * 100) / progressEvent.total
-    //     );
-    //     onProgress(percentage);
-    //   }
-    // },
-  };
-  return axiosInstance.post(`${ backendHost }/api/files`, formData, config).then(response => response.data)
+    onUploadProgress: onProgress
+  }).then(response => response.data)
 }
--- frontend/src/pages/files.tsx
+++ frontend/src/pages/files.tsx
@@ -1,22 +1,36 @@
-import { ChangeEvent } from 'react'
+import { ChangeEvent, useState } from 'react'
 import { uploadFiles } from '../api/file.ts'
 
 
 const Files = () => {
+  const [ uploadProgress, setUploadProgress ] = useState<number | undefined>(undefined)
+  const [ currentUploadAbortController, setCurrentUploadAbortController ] = useState<AbortController | undefined>()
 
-  const handleChange =async (e: ChangeEvent<HTMLInputElement>) => {
+  const handleChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const files = e.target.files
+
     if (files) {
       for (const file of files) {
-        await uploadFiles(file)
+        const abortController = new AbortController()
+        setCurrentUploadAbortController(abortController)
+        await uploadFiles(file, (progress) => setUploadProgress(progress.progress), abortController)
       }
     }
+    // @ts-ignore
+    e.target.value = null
   }
+
 
   return (<>
       <div className="filesContainer">
-      <input type="file" multiple onChange={handleChange}/>
+        <input type="file" multiple onChange={ handleChange }/>
       </div>
+      <p>{ uploadProgress ? Math.floor(uploadProgress * 100) + ' %' : '' }</p>
+      <button onClick={ () => {
+        currentUploadAbortController?.abort()
+        setUploadProgress(undefined)
+      } }>Cancel
+      </button>
     </>
   )
 
