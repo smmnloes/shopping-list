91ea0b5ed541483ce141722094d7848c8f61cdee
Maximilian Lösch <m.loesch@hotmail.de>
2025-03-02 23:01:51 +0100

    insult styles

--- frontend/src/pages/insult-view.tsx
+++ frontend/src/pages/insult-view.tsx
@@ -1,16 +1,32 @@
 import '../styles/insults.scss'
-
-const nrOfAnimations = 3
-const getRandomAnimation = () => Math.floor(Math.random() * nrOfAnimations + 1)
+import { CSSProperties } from 'react'
 
 
 const InsultView = () => {
 
   const params = new URLSearchParams(location.search)
+  const insult = params.get('insult')
+  const nrOfAnimations = 5
+  const singleLetterAnimations = [ 4, 5 ]
+  const randomAnimation = Math.floor(Math.random() * nrOfAnimations + 1)
+
+  const randomRGB = Math.floor(Math.random() * 3)
 
   return (<>
       <div className="insult-view-wrapper">
-        <div className={`insult-view text-animation-${getRandomAnimation()}`}>{ params.get('insult') }</div>
+        <div
+          className={ `insult-view text-animation-${ randomAnimation }` }>
+          { singleLetterAnimations.includes(randomAnimation)
+            ? insult?.split('').map((char, index) => <span key={ index }
+                                                           style={ {
+                                                             '--index': index,
+                                                             '--randomR': randomRGB === 0 ? 1 : 0,
+                                                             '--randomG': randomRGB === 1 ? 1 : 0,
+                                                             '--randomB': randomRGB === 2 ? 1 : 0,
+                                                             '--randomVal': Math.random()
+                                                           } as CSSProperties }>{ char }</span>)
+            : insult }
+        </div>
       </div>
     </>
   )
