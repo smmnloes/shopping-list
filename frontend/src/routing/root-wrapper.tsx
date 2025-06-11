ebc514729c5ab33bd03f5fb0ce4909a5461245de
Maximilian Lösch <m.loesch@hotmail.de>
2025-06-11 21:53:10 +0200

    make whatsapp happy

--- frontend/src/routing/root-wrapper.tsx
+++ frontend/src/routing/root-wrapper.tsx
@@ -6,6 +6,14 @@ import Layout from '../elements/layout.tsx'
 
 export const RootWrapper = ({ children, privateRoute }: { privateRoute: boolean, children: React.JSX.Element }) => {
 
+  if (document.location.hostname === 'share.mloesch.it') {
+    document.title = 'Dateifreigabe'
+  } else if (document.location.hostname === 'shopping.mloesch.it') {
+    document.title = 'Einkaufsliste'
+  } else if (document.location.hostname === 'localhost') {
+    document.title = 'Einkaufsliste - local'
+  }
+
   const wrappedChildren = privateRoute ? (<PrivateRoute>{ children }</PrivateRoute>) : children
 
   return <AuthProvider>
